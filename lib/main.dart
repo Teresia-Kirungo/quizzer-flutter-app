27a0eaa97c200b101ba494386de285111915611a
Teresia-Kirungo <terrykirungo@gmail.com>
2021-05-03 17:48:33 +0300

    Updated lists

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -53,43 +61,62 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-              child: Text('True'),
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.green,
-                onSurface: Colors.grey,
-                textStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+                child: Text('True'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.green,
+                  onSurface: Colors.grey,
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              onPressed: () {
-                print('The user pressed True');
-              },
-            ),
+                onPressed: () {
+                  //User pressed True
+                  bool correctAnswer = answers[questionNumber];
+                  if (correctAnswer == true) {
+                    print('User got it right!');
+                  } else {
+                    print('User got it wrong!');
+                  }
+                  setState(() {
+                    questionNumber++; //increments question number by 1
+                  });
+                }),
           ),
         ),
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-              child: Text('False'),
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.red,
-                onSurface: Colors.grey,
-                textStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+                child: Text('False'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.red,
+                  onSurface: Colors.grey,
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              onPressed: () {
-                print('The user pressed false');
-              },
-            ),
+                onPressed: () {
+                  //User pressed True
+                  bool correctAnswer = answers[questionNumber];
+                  if (correctAnswer == false) {
+                    print('User got it right!');
+                  } else {
+                    print('User got it wrong!');
+                  }
+                  setState(() {
+                    questionNumber++; //increments question number by 1
+                  });
+                }),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
